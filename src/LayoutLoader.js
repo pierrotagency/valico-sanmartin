1b47019ab349c79b5a4a890057c889b0ecfd8286
rodrigobutta <rbutta@gmail.com>
2020-01-06 13:07:04 -0300

    layout styling

--- src/LayoutLoader.js
+++ src/LayoutLoader.js
@@ -3,9 +3,11 @@ import Layout2 from './layouts/Layout2'
 
 export const layouts = {
   Layout1: {
+    name: 'Layout 1',
     view: Layout1
   },
   Layout2: {
+    name: 'Layout 2',
     view: Layout2
   }
 }
